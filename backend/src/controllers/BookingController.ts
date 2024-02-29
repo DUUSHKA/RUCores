4805d0c467ee7e106e1a303dbe32980a57dee81b
Varsini <varsini.dhinakaran2019@gmail.com>
2024-02-29 16:36:05 -0500

    booking and availability controllers

--- backend/src/controllers/BookingController.ts
+++ backend/src/controllers/BookingController.ts
@@ -1,12 +1,27 @@
-import { CurrentUser, Get, JsonController } from "routing-controllers";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Authorized,
+  Body,
+  CurrentUser,
+  Delete,
+  Get,
+  HttpCode,
+  JsonController,
+  OnUndefined,
+  Param,
+  Post,
+  Put,
+} from "routing-controllers";
 import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
 import { BookingEntity } from "../database/Entities/bookingEntity";
 import { UserEntity } from "../database/Entities/userEntity";
 import { auth_errors } from "../documentation/common";
 import BookingService from "../services/BookingService";
+import { BookingModel } from "../types/BookingModel";
 
-@JsonController("/bookings")
+@JsonController("/booking")
 @OpenAPI(auth_errors)
+@Authorized()
 export class BookingController {
   service: BookingService;
 
@@ -14,14 +29,55 @@ export class BookingController {
     this.service = new BookingService();
   }
 
-  @Get("/current")
+  @Get("/scheduled")
+  @HttpCode(200)
   @OpenAPI({
-    description: "Get all bookings for the current user",
+    summary: "Get all bookings scheduled by the current user",
   })
   @ResponseSchema(BookingEntity, { isArray: true })
-  public async getAllForUser(
-    @CurrentUser() user: UserEntity,
-  ): Promise<BookingEntity[]> {
+  async getManaged(@CurrentUser() user: UserEntity): Promise<BookingEntity[]> {
     return user.bookings;
   }
+
+  @Post()
+  @HttpCode(201)
+  @OpenAPI({
+    summary: "Create a new booking",
+  })
+  @ResponseSchema(BookingEntity)
+  post(
+    @CurrentUser() user: UserEntity,
+    @Body() booking: BookingModel,
+  ): Promise<BookingEntity> {
+    return this.service.createBooking(user, booking);
+  }
+
+  @Put("/:id")
+  @HttpCode(200)
+  @OpenAPI({
+    summary: "Update a booking",
+  })
+  @ResponseSchema(BookingEntity)
+  put(
+    @CurrentUser() user: UserEntity,
+    @Param("id") id: number,
+    @Body({
+      validate: { forbidUnknownValues: true, skipMissingProperties: true },
+    })
+    booking: BookingModel,
+  ): Promise<BookingEntity> {
+    return this.service.updateBooking(user, id, booking);
+  }
+
+  @Delete("/:id")
+  @HttpCode(204)
+  @OpenAPI({
+    summary: "Delete a booking",
+  })
+  @OnUndefined(204)
+  remove(@CurrentUser() user: UserEntity, @Param("id") id: number) {
+    return this.service.deleteBooking(user, id);
+  }
 }
+
+export default BookingController;
