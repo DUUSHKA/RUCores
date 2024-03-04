239457507fd86e5c869d0bd48887e120d109a804
Sahmi Abubakar <sahmi.abubakar@gmail.com>
2024-03-03 21:47:32 -0500

    added login fetch

--- frontend/rucores/src/loginPage/login.js
+++ frontend/rucores/src/loginPage/login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +7,37 @@ import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 
 function Login() {
+
+  // const [isRequestSent, setIsRequestSent] = useState(false);
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  
+
+
+
+  const handlePostRequest = async () => {
+    try {
+      const postData = {
+      "username": {username},
+      "password": {password}};
+      console.log(postData);
+      const response = await fetch('http://localhost:3001/api/users/login', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify(postData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create post');
+      }
+
+      // Handle the response data if needed
+      const result = await response.json();
+      console.log('Post created successfully:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
     /**
      * set background color to red for login button
@@ -33,6 +64,7 @@ function Login() {
                         placeholder="Username"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
+                        onChange={(e) =>(setUsername(e.target.value))}
                     />
                 </InputGroup>
                 <InputGroup className="passwordInput">
@@ -41,17 +73,20 @@ function Login() {
                         placeholder="Password"
                         aria-label="Password"
                         aria-describedby="basic-addon1"
+                        onChange={(e) =>(setPassword(e.target.value))}
                     />
                 </InputGroup>
                 <Link to="/CreateAccountPage">
                     <Button variant="link">{"Create account"}</Button>
                 </Link>
 
-
+                <Button style={buttonColor} onClick={handlePostRequest}>Login</Button>
                 <Link to="/dashboard">
-                    <Button style={buttonColor}>Login</Button>
+                    
                 </Link>
             </div>
+            {username}
+            {password}
         </>
     );
 
